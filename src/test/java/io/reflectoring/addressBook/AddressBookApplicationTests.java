c253ca00b62477caca4aede9efe5b1273d228ee0
shrey <shreya272002gupta@gmail.com>
2025-03-07 12:52:13 +0530

    [Shreya] Address Book setup

---
+++ src/test/java/io/reflectoring/addressBook/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.addressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
