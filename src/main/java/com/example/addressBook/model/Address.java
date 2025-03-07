3400a9d0b758b2c5490f05252acc714c4c0f582b
shrey <shreya272002gupta@gmail.com>
2025-03-07 13:28:00 +0530

    [Shreya] Add medel in uc2

--- src/main/java/com/example/addressBook/model/Address.java
+++ src/main/java/com/example/addressBook/model/Address.java
@@ -1,4 +1,70 @@
-package io.reflectoring.addressBook.model;
+package com.example.addressBook.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+
+    // No-args constructor
+    public Address() {
+    }
+
+    // All-args constructor
+    public Address(Long id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // toString() method
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
