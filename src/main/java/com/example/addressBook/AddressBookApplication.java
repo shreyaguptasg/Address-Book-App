a560fb845b1df60ce99b58ea2d088bf4354e0e20
shrey <shreya272002gupta@gmail.com>
2025-03-07 13:14:32 +0530

    [Shreya] Add. Address Book setup

--- src/main/java/com/example/addressBook/AddressBookApplication.java
+++ src/main/java/com/example/addressBook/AddressBookApplication.java
@@ -1,4 +1,4 @@
-package io.reflectoring.addressBook;
+package com.example.addressBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
