daa22e8754b5899ea3be83dc9fa305338447948a
shrey <shreya272002gupta@gmail.com>
2025-03-07 13:28:23 +0530

    [Shreya] Add repository in uc2

--- src/main/java/com/example/addressBook/repository/AddressRepository.java
+++ src/main/java/com/example/addressBook/repository/AddressRepository.java
@@ -1,4 +1,9 @@
-package io.reflectoring.addressBook.repository;
+package com.example.addressBook.repository;
 
-public class AddressRepository {
+import com.example.addressBook.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }
